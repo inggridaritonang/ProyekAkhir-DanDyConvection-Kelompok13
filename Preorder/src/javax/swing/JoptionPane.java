2078f01feb6fdd842f76e13b980d0cd3ab4b5480
@inggridaritonang <60682346+inggridaritonang@users.noreply.github.com>
2020-06-08 17:38:27 +0700

    Add files via upload

---
+++ Preorder/src/javax/swing/JoptionPane.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javax.swing;
+
+/**
+ *
+ * @author Kelompok 13
+ */
+public class JoptionPane {
+
+    public static void showMessageDialog(Object object, String maaf_tidak_bisa) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
